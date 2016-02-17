1d78d532b4e2b04674fd114acfbe116e64d26b34
home <natalia.l.kushnir@gmail.com>
2016-02-17 14:50:29 +0200

    done 1st task (added decorator for setting static var, created queryset for request)

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+from main.decorators import static_vars
+from main.models import ListObject
+
+
+@static_vars(obj_index=0)
+def index(request):
+    if index.obj_index >= ListObject.objects.count():
+        index.obj_index = 0
+        obj = ListObject.objects.filter(processed=False)[index.obj_index]
+        index.obj_index += 1
+    else:
+        obj = ListObject.objects.filter(processed=False)[index.obj_index]
+        index.obj_index += 1
+    print('ID: ' + str(obj.id) + ', Name: ' + obj.name)
+    return HttpResponse(request)
